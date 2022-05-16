9c7f777409a9b0d23c4d0e102f1ab450bdd085de
SeungMinKim <ksm8797@naver.com>
2022-05-17 02:09:15 +0900

    TEST : DB 연결 테스트
    DB 정상 연결 테스트

--- server/server/routes/main.js
+++ server/server/routes/main.js
@@ -6,7 +6,21 @@ const logger = require("../../public/js/logger");
 const exec_sql = require("../../public/js/exec_sql");
 
 router.get("/test", function (req, res) {
-  res.send("정상 연결");
+  const selectQuery = `select * from TBL_CATEGORY`;
+  getConnection((connection) => {
+    exec_sql(connection, selectQuery)
+      .then((response) => {
+        console.log(response[0].CGR_ID);
+
+        res.send(`정상 연결  : ${response[0].CGR_ID}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        connection.release();
+      });
+  });
 });
 
 module.exports = router;
