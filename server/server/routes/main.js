1664149646e0e49b3ae9895af53f609de1318005
SeungMinKim <ksm8797@naver.com>
2022-05-17 23:01:57 +0900

    FEAT : inquiry 라우터 추가
    - TBL_CATEGORY, ORDER, ITEM 에서 데이터 SELECT 해서 JSON 형식으로 SEND

--- server/server/routes/main.js
+++ server/server/routes/main.js
@@ -9,20 +9,57 @@ const ACCESS_TOKEN = process.env.ACCESS_TOKEN; // ACCESS_TOKEN
 require("dotenv").config();
 
 router.post("/inquiry", function (req, res) {
+  /*사용자 검증 */
   const accessToken = req.headers.authorization.split(" ")[1];
-
   if (accessToken === ACCESS_TOKEN) {
-    const selectDataQuery = `
-        select c.CGR_NM,c.CGR_ID,o.ORDER_ID,i.ITEM_ID  from TBL_CATEGORY c 
-        LEFT JOIN TBL_ORDER o ON c.CGR_ID = o.CGR_ID 
-        LEFT JOIN TBL_ITEM i ON o.ORDER_ID  = i.ORDER_ID
+    /*Authorization*/
+
+    /*LEFT JOIN Version */
+    // const selectDataQuery = `
+    //     select COUNT(c.CGR_NM) COUNT,c.CGR_NM,c.CGR_ID,o.ORDER_ID,i.ITEM_ID  from TBL_CATEGORY c
+    //     LEFT JOIN TBL_ORDER o ON c.CGR_ID = o.CGR_ID
+    //     LEFT JOIN TBL_ITEM i ON o.ORDER_ID  = i.ORDER_ID
+    //     group by c.CGR_NM
+    //     HAVING  COUNT(c.CGR_NM) >= 1
+    //     `;
+    // getConnection((connection) => {
+    //   (async () => {
+    //     try {
+    //       const inquiryData = await exec_sql(connection, selectDataQuery);
+    //       if (inquiryData) res.status(200).send(inquiryData);
+    //       else res.status(400).send(false);
+    //     } catch (err) {
+    //       console.log(err);
+    //     } finally {
+    //       connection.release();
+    //     }
+    //   })();
+    // });
+
+    /*Multi Query Version*/
+    const categoriesDataQuery = `
+        select COUNT(c.CGR_NM) COUNT,c.CGR_NM, c.CGR_ID from TBL_CATEGORY c
+         LEFT JOIN TBL_ORDER o ON c.CGR_ID = o.CGR_ID
+         LEFT JOIN TBL_ITEM i ON o.ORDER_ID  = i.ORDER_ID
+         group by c.CGR_NM
+         HAVING  COUNT(c.CGR_NM) >= 1
+        `;
+
+    const orderDataQuery = `
+           select CGR_ID,ORDER_ID from TBL_ORDER;
+        `;
+
+    const itemDataQuery = `
+           select ORDER_ID,ITEM_ID from TBL_ITEM;
         `;
 
     getConnection((connection) => {
       (async () => {
         try {
-          const inquiryData = await exec_sql(connection, selectDataQuery);
-          if (inquiryData) res.status(200).send(inquiryData);
+          const categoriesData = await exec_sql(connection, categoriesDataQuery);
+          const orderData = await exec_sql(connection, orderDataQuery);
+          const itemData = await exec_sql(connection, itemDataQuery);
+          if (categoriesData) res.status(200).send({ categoriesData, orderData, itemData });
           else res.status(400).send(false);
         } catch (err) {
           console.log(err);
@@ -32,6 +69,7 @@ router.post("/inquiry", function (req, res) {
       })();
     });
   } else {
+    /*unAuthorization*/
     res.status(401).send(false);
   }
 });
