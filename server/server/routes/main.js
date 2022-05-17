b7f888342a70075fb253c20b9572381c09a2b6fe
SeungMinKim <ksm8797@naver.com>
2022-05-17 20:07:02 +0900

    FEAT : Add ACCESS_TOKEN
    - Hearders에 ACCESS TOKEN을 보내지 않으면 API 사용 불가

--- server/server/routes/main.js
+++ server/server/routes/main.js
@@ -5,22 +5,30 @@ const getConnection = require("../../lib/db");
 const logger = require("../../public/js/logger");
 const exec_sql = require("../../public/js/exec_sql");
 
-router.post("/test", function (req, res) {
-  const selectQuery = `select * from TBL_CATEGORY`;
-  getConnection((connection) => {
-    exec_sql(connection, selectQuery)
-      .then((response) => {
-        console.log(response[0].CGR_ID);
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN; // ACCESS_TOKEN
+require("dotenv").config();
 
-        res.send(`정상 연결  : ${response[0].CGR_ID}`);
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => {
-        connection.release();
-      });
-  });
+router.post("/inquiry", function (req, res) {
+  const accessToken = req.headers.authorization.split(" ")[1];
+
+  if (accessToken === ACCESS_TOKEN) {
+    const selectQuery = `select * from TBL_CATEGORY`;
+    getConnection((connection) => {
+      exec_sql(connection, selectQuery)
+        .then((response) => {
+          if (response[0]) res.status(200).send(response);
+          else res.status(400).send(response);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          connection.release();
+        });
+    });
+  } else {
+    res.status(401).send(false);
+  }
 });
 
 module.exports = router;
